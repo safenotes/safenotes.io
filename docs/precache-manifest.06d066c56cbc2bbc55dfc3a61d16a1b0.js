3319afb452163c3bda9141a2f0f6e454611bd5e1
Adam <adam@example.com>
2020-06-11 13:48:41 +0800

    Update

---
+++ docs/precache-manifest.06d066c56cbc2bbc55dfc3a61d16a1b0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b728b4c838fbb0c8d2517c41998781b",
+    "url": "/404.html"
+  },
+  {
+    "revision": "952c70b03e7bdc5129edcd579e4fcc81",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "95b9185565555f2b4405dff033ce56c8",
+    "url": "/_config.yml"
+  },
+  {
+    "revision": "b504a34382d56ff041de",
+    "url": "/css/app.405883dd.css"
+  },
+  {
+    "revision": "017c16c9f643450e899f",
+    "url": "/css/chunk-vendors.7284e7b3.css"
+  },
+  {
+    "revision": "2dcce27160495d68abf4945acd282448",
+    "url": "/fonts/materialdesignicons-webfont.2dcce271.woff"
+  },
+  {
+    "revision": "3e2c1c7919fb45d5dee5b0703fe52931",
+    "url": "/fonts/materialdesignicons-webfont.3e2c1c79.eot"
+  },
+  {
+    "revision": "a323c28ecd42189e70efca274f5b7089",
+    "url": "/fonts/materialdesignicons-webfont.a323c28e.woff2"
+  },
+  {
+    "revision": "e7dec9c5e1bd830c084f2d2fb94fa1e7",
+    "url": "/fonts/materialdesignicons-webfont.e7dec9c5.ttf"
+  },
+  {
+    "revision": "2ad0171d04f6988f01594b45605ff5b2",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "a6fbb5a65832466a4be8c8642f0567e2",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "277c018a756fa5a23386586e6d739778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b504a34382d56ff041de",
+    "url": "/js/app.f8fe3060.js"
+  },
+  {
+    "revision": "017c16c9f643450e899f",
+    "url": "/js/chunk-vendors.9feee949.js"
+  },
+  {
+    "revision": "fc79a68db5a6f81e3e0fab14d16121ad",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "762cca78234bdf08c9830e7a06f30dab",
+    "url": "/service-worker.js"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,32 @@
+importScripts("/precache-manifest.06d066c56cbc2bbc55dfc3a61d16a1b0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable */
+
+if (workbox) {
+  console.log(`Workbox is loaded`)
+
+  workbox.precaching.precacheAndRoute(self.__precacheManifest)
+} else {
+  console.log(`Workbox didn't load`)
+}
+
+// install new service worker when ok, then reload page.
+self.addEventListener('message', (msg) => {
+  if (msg.data.action == 'skipWaiting') {
+    self.skipWaiting()
+  }
+})
+
+// Close all notifications
+self.addEventListener('message', (msg) => {
+  if (msg.data.action != 'closeNotifications') {
+    return
+  }
+
+  self.registration.getNotifications().then((notifications) => {
+    notifications.forEach((notification) => {
+      notification.close()
+    })
+  })
+})
+
